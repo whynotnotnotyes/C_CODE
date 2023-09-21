9f17af7ce17f6b4dd0121e6296ccd079fed7f12e
whynotnotnotyes <143332822+whynotnotnotyes@users.noreply.github.com>
2023-09-21 16:38:13 +0800

    first
    first

---
+++ 第二次勇敢牛牛.c
@@ -0,0 +1,105 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//一
+//int main()
+//{
+//
+//	int MAX = 100000;
+//
+//	int a = MAX;
+//
+//	printf("a = %d\n", a);
+//
+//	return 0;
+//
+//}
+//
+//
+////二.枚举常量
+//enum sex
+//{
+//	male = 3,
+//	female,
+//	secret
+//
+//};
+//
+//
+//int main()
+//{
+//	printf("%d\n", female);
+//	printf("%d\n", male);
+//	printf("%d\n", secret);
+//
+//
+//}
+//
+//
+//三
+//int main() 
+//{
+//	const int a = 12;
+//
+//
+//	printf("%d\n", a);
+//
+//	return 0;
+//
+//
+//}
+//
+//
+//四
+//int main()
+//
+//{
+//
+//	/*char arr1[] = "abc";
+//
+//	char arr2[] = { 'a','b','c','\0'};
+//
+//	printf("% s\n", arr1);
+//
+//	printf("%s\n", arr2);
+//
+//	
+//
+//	printf("%d\n", strlen(arr1));
+//
+//	printf("%d\n", strlen(arr2));*/
+//
+//	int a = 199999;
+//
+//	int len = strlen("a");
+//
+//	printf("%d\n", len);
+//
+//	printf("%d\n", strlen(a));
+//
+//	strlen:你输入是什么，它就会输出多少位的数，而不是输出那个变量所包含的大小，比如strlen（a），输出的就是1，strlen（inta），输出的就是4；
+//	 
+//	int len = strlen("arr1");
+//
+//	printf("%d\n", len);
+//
+//	
+//	char a = 'n';
+//
+//	printf("%s\n", a);
+
+
+
+	int main()
+	{
+		int a = 0;
+
+		printf("%d\n", a);
+
+		return 0;
+
+	}
+
+
+
