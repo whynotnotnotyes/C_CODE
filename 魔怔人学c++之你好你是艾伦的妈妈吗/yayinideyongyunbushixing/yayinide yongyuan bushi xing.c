8e0524d92a997ff46392c9ad6a6f7b5d913bdbb6
whynotnotnotyes <143332822+whynotnotnotyes@users.noreply.github.com>
2024-05-06 18:29:22 +0800

    2024.5.6
    2024.5.6

--- 魔怔人学c++之你好你是艾伦的妈妈吗/yayinideyongyunbushixing/yayinide yongyuan bushi xing.c
+++ 魔怔人学c++之你好你是艾伦的妈妈吗/yayinideyongyunbushixing/yayinide yongyuan bushi xing.c
@@ -14237,7 +14237,7 @@ double的scanf用 %lf ，printf用 %f ， %e（科学计数法）	*/
 
 
 
-//章节：字符串函数与刨析
+//新章节：字符串函数与刨析
 
 //求字符串长度
 //strlen
@@ -14496,6 +14496,8 @@ double的scanf用 %lf ，printf用 %f ， %e（科学计数法）	*/
 
 //strcmp
 //字符串比较函数
+//int strcmp( const char *string1, const char *string2 );
+
 
 //#include <stdio.h>
 //
@@ -14591,8 +14593,1366 @@ double的scanf用 %lf ，printf用 %f ， %e（科学计数法）	*/
 
 
 
+//以上函数都是长度不受限制的字符串函数,都是以\0为结束标志
+//所以相对不够安全,举个例子
+
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	char arr1[5] = "abc";
+//	char arr2[] = "hello,world";
+//
+//	strcpy(arr1, arr2);
+//
+//	printf("%s\n", arr1);
+//
+//	return 0;
+//}
+
+//这样会报错，因为arr1只能存放5个元素,强行把arr2 11个元素放进去而且是以\0结尾，会放不下
 
 
 
 
 
+//下面介绍受长度限制的字符串库函数
+
+
+
+
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	char arr1[10] = "abcdefg";
+//	char arr2[] = "hello";
+//
+//	strncpy(arr1, arr2,4);
+//	//strncpy不会把\0拷贝过去，是根据size_t拷贝,size_t是多少就拷贝多少个字节
+//
+//	printf("%s\n\n", arr1);
+//
+//
+//	char arr3[10] = "abcdefgh";
+//	char arr4[] = "abc";
+//	
+//	strncpy(arr3, arr4,6);
+//	//这种情况源地址根本没有6个字节，不够的话就会自动补成\0,而不是把\0复制过去
+//	//6-3 = 3,后面追加3个\0补全
+//
+//	printf("%s\n\n", arr3);
+//
+//	return 0;
+//}
+
+//strncpy在MSDN的解释,count是拷贝多少个字节的意思，单位是字节
+//char *strncpy( char *strDest, const char *strSource, size_t count );
+
+//strcpy在MSDN的解释
+//char *strcpy( char *strDestination, const char *strSource );
+
+
+
+
+
+
+//模拟实现strncpy
+
+//#include <stdio.h>
+//
+//char* my_strncpy(char* str1, char* str2, int k)
+//{
+//	char* ret = str1;
+//	int count = 0;
+//
+//	while (count < k)
+//	{
+//		if (*str2 == '\0')
+//		{
+//			*str1 = '\0';
+//		}
+//		else
+//		{
+//			*str1 = *str2;
+//		}
+//
+//		str1++;
+//		str2++;
+//		count++;
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	char arr1[10] = "abcdefghi";
+//	char arr2[] = "ABCDE";
+//
+//	my_strncpy(arr1, arr2, 10);
+//
+//	printf("%s\n", arr1);
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+//strncat
+//MSDN里strncat的解释
+//char *strncat( char *strDest, const char *strSource, size_t count );
+
+//模拟实现strncat
+
+//#include <stdio.h>
+//#include <assert.h>
+//
+//char* my_strncat(char* arr1, char* arr2, int k)
+//{
+//	assert(arr1 && arr2);
+//
+//	char* ret = arr1;
+//
+//	while (*arr1 != '\0')
+//	{
+//		arr1++;
+//	}
+//
+//	int count = 0;
+//	while (count <= k)
+//	{
+//		if (*arr2 == '\0')
+//		{
+//			*arr1 = '\0';
+//			return 0;
+//		}
+//		else
+//		{
+//			*arr1 = *arr2;
+//		}
+//		
+//		arr1++;
+//		arr2++;
+//		count++;
+//	}
+//
+//	return ret;
+//}
+//
+//int main()
+//{
+//	char arr1[30] = "abcdef\0xxxxxxxxxxx";
+//	char arr2[] = "ABCDEF";
+//
+//	my_strncat(arr1, arr2, 10);
+//	//strncat得把\0追加过去,当想要追加的数大于源地址大小时，后面只会补一个\0，strncpy会补全\0
+//
+//	printf("%s\n", arr1);
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+//字符串比较函数
+//strncmp
+//strncmp在MSDN里的解释
+//int strncmp( const char *string1, const char *string2, size_t count );
+
+
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	const char* arr1 = "abcdef";
+//	const char* arr2 = "abzdef";
+//
+//	int ret = strncmp(arr1, arr2, 3);
+//
+//	printf("%d\n", ret);
+//
+//	return 0;
+//}
+
+
+
+
+
+
+//模拟实现strncmp
+
+//#include <stdio.h>
+//
+//int my_strncmp(char* arr1, char* arr2, int k)
+//{
+//	int count = 0;
+//
+//	while (count < k)
+//	{
+//		if (*arr1 > *arr2)
+//		{
+//			return 1;
+//		}
+//		else if (*arr1 < *arr2)
+//		{
+//			return -1;
+//		}
+//
+//		arr1++;
+//		arr2++;
+//		count++;
+//	}
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	const char* arr1 = "abcdefg";
+//	const char* arr2 = "abcdzfg";
+//
+//	int ret = my_strncmp(arr1, arr2, 5);
+//
+//	printf("%d\n", ret);
+//
+//	return 0;
+//}
+
+
+
+
+
+
+//strstr-查找子字符串函数
+//strstr只会返回两种情况: NULL空指针 和 地址
+//找到了子串返回查找的子串的首元素地址，没找到返回NULL
+
+//在MSDN文档里NULL表示空指针
+//NUL/Null都表示 \0
+
+//NULL - 空指针
+//NUL/Null - \0
+
+//MSDN里strstr的解释：
+//char* strstr(const char* string, const char* strCharSet);
+
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	char arr1[] = "abcdefhi";
+//	char arr2[] = "def";
+//
+//	char* ret = strstr(arr1, arr2);
+//
+//	if (ret == NULL)
+//	{
+//		printf("没找到\n");
+//	}
+//	else
+//	{
+//		printf("%s\n",ret);
+//		//传回来的是子串首元素的地址,也就是d,打印的是defhi,因为没有遇到\0就会一直打印
+//		//如果被查找字符串是abcdefdef,要查找的字符串是def，这里有两份def,返回的是第一份子串的地址,
+//		//也就是会打印defdef
+//	}
+//
+//	return 0;
+//}
+
+
+
+
+//模拟实现my_strstr
+
+//空字符串" "里面只有一个元素,那就是 '\0'
+
+//#include <stdio.h>
+//#include <assert.h>
+//
+//char* my_strstr(const char* p1,const char* p2)
+//{
+//	assert(p1 && p2);
+//
+//	char* s1 = (char*)p1;
+//	char* s2 = (char*)p2;
+//	char* cur = (char*)p1;
+//
+//	//若p2传过来的是空字符串" ",里面只有一个元素,是 \0,那就返回p1的地址,返回什么看你自己怎么写
+//	//写成printf("传过来的是空字符串\n") 也可以
+//	if (*p2 == '\0')
+//	{
+//		return (char*)p1;  //为什么要强制类型转换,因为p1是const char*类型,返回类型是char类型,会有差异
+//	}
+//
+//	while (*cur)
+//	{
+//		s1 = cur;
+//		s2 = (char*)p2;
+//
+//		while ((*s1 != '\0') && (*s2 != '\0') && (*s1 == *s2))
+//		{
+//			s1++;
+//			s2++;
+//		}
+//		if (*s2 == '\0')   //如果*s2=='\0',这里其实已经蕴含了几种条件,那就是1.s2已经查找完；2. s1和s2同时一一对应并且s2查找完
+//		{
+//			return cur;	//找到子串
+//		}
+//		cur++;
+//	}
+//	return NULL; //没找到子串
+//}
+//
+//int main()
+//{
+//	const char* arr1 = "abbbcdr\0ef";
+//	const char* arr2 = "bbc";
+//
+//	char* ret = my_strstr(arr1, arr2);
+//
+//	if (ret == NULL)
+//	{
+//		printf("没找到\n\n");
+//	}
+//	else
+//	{
+//		printf("%s\n", ret);
+//	}
+//
+//	return 0;
+//}
+
+
+//这代码的精髓在于cur的设置,每次只过一个元素，从这个元素开始查找，找不到再过下一个元素
+
+
+
+
+
+
+
+
+
+//strtok - 字符串分割函数
+
+//strtok在MSDN里的解释:
+//char* strtok(char* str, const char* sep);
+//sep参数是个字符串，定义了用作分隔符的字符集合
+
+
+//第一个参数指定一个字符串，它包含了0个或者多个由sep字符串中一个或者多个分隔符分割的标记
+//strtok函数找到str中的下一个标记,并将用\0结尾，返回一个指向这个标记的指针(注：strtok函数
+// 会改变被操作的字符串，所以在使用strtok函数切分的字符串一般都是临时拷贝的内容并且可修改)
+//strtok函数的第一个参数不为NUL，函数将找到str中第一个标记,strtok函数将保存它在字符串中的位置
+//strtok函数的第一个参数为NULL，函数将在同一个字符串中被保存的位置开始，查找下一个标记
+//如果字符串中不存在更多的标记，则返回NULL指针
+
+
+//char arr[] = "edu@bit.tech";
+//char* p = "@.";
+//strtok(arr,p);
+
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	char arr[] = "edu@bit.tech";
+//	char* p = "*";
+//	
+//	char buf[255] = { 0 };
+//	strcpy(buf, arr);
+//	//为什么要再搞一个变量呢？因为strtok会真实改变arr的内容，所以搞一个拷贝
+//
+//
+//	char* ret = strtok(buf, p);
+//	printf("%s\n", ret);
+//
+//	ret = strtok(NULL, p);
+//	printf("%s\n", ret);
+//
+//	ret = strtok(NULL, p);
+//	printf("%s\n", ret);
+//	//strtok第一个参数不为NULL的情况,strtok将找到分隔符，并把分隔符改成\0(真正的改变),然后返回最开头那个字符的地址:edu
+//	//再次调用的时候，str就不是arr了,改成NULL，即可再次往后使用
+//	//如果字符串中不存在更多的标记，就会返回NULL
+//	//strtok一次只能切一段
+//
+//	return 0;
+//}
+
+
+
+
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	char arr[] = "www.baidu.com@.@edu";
+//
+//	char* p = "@.";
+//
+//	char* ret = NULL;
+//
+//	for (ret = strtok(arr, p); ret != NULL; ret = strtok(NULL, p))
+//	{
+//		printf("%s\n", ret);
+//	}
+//
+//	return 0;
+//}
+
+//把分割符号改成\0之后，后面参数不用给arr给NULL就行
+//这个函数是怎么从第一个分割往后找的呢?可以推测有一个静态变量
+
+
+
+
+
+
+
+
+
+
+
+//strerror - 错误报告函数(错误信息函数)
+
+//这些都是错误码 ， 每一个错误码对应一个错误信息
+//0 - No error
+//1 - Operation not permitted
+//2 - No such file or directory
+//3 - No such process
+
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//	//char* str = strerror(3);  - 这只是演示正常不这么用
+//
+//	//应该是这样用的
+//	char* str = strerror(errno);
+//	//errno 是一个全局的错误码的变量
+//	//当C语言的库函数在执行过程中，发生了错误,就会把对应的错误码，赋值到errno中
+//	//此时strerror再根据错误码转换成错误信息
+//
+//	printf("%s\n", str);
+//
+//	return 0;
+//}
+
+//strerror的作用就是转换我们的错误码，把错误码翻译成错误信息,并返回它的地址
+
+
+
+
+//#include <stdio.h>
+//#include <errno.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	//C语言里可以打开文件，用fopen函数.fopen("文件名","r"),r的意思是读文件
+//	//fopen会返回一个FILE*的指针
+//	//如果这个函数调用失败了，会返回一个空指针NULL放到pf里面
+//	//但是打开文件失败有很多种原因：文件不存在，打开权限不够等等原因,
+//	//想知道具体原因的话就得strerror(errno)
+//	FILE* pf = fopen("test.txt", "r");
+//
+//	if (pf == NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//	}
+//	else
+//	{
+//		printf("open file success\n");
+//	}
+//
+//	return 0;
+//}
+
+
+
+//以上函数叫做字符串函数
+
+
+
+//字符分类函数 
+//用字符分类函数需要引 <ctype.h>的头文件
+//常见的字符分了函数：tolower(转小写函数)  toupper(转大写函数)
+
+//#include <stdio.h>
+//#include <ctype.h>
+//
+//int main()
+//{
+//	char ch = tolower('Q');
+//	char ui = toupper('m');
+//
+//	putch(ch);
+//	printf("\n");
+//	putch(ui);
+//
+//	return 0;
+//}
+
+
+
+
+//把一个大写字符串全部改成小写
+
+//#include <stdio.h>
+//#include <ctype.h>
+//
+//int main()
+//{
+//	char arr[] = "I AM A STUDENT";
+//
+//	int i = 0;
+//
+//	while (arr[i])
+//	{
+//		arr[i] = tolower(arr[i]);
+//		//修改的值要放回arr里面
+//		i++;
+//	}
+//
+//	printf("%s\n", arr);
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+//内存函数
+//memcpy  - 内存拷贝函数
+//memmove - 内存移动函数
+//memcmp  - 内存比较函数
+//memset
+
+//上面学的都是字符串函数，操作对象是字符串，这些函数或多或少都要和\0打交道
+//对整型数组，字符串数组，结构体都用不了
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[] = { 0 };
+//
+//	strcpy(arr2, arr1);
+//
+//	return 0;
+//}
+
+//arr是int型数组，在内存中的存储是：
+//01 00 00 00 02 00 00 00 03 00 00 00 04 00 00 00 00 05 00 00 00 00
+//strcpy会一个字符一个字符去拷贝，先拷贝第一个字符01，01后面是00，strcpy遇到\0停止，于是就停止了
+//所以strcpy只能拷贝字符串，有局限性,对于这种问题，就有内存操作函数的出现
+
+
+
+
+
+
+
+//memcpy - 内存拷贝函数
+//只要是内存中存放的数据，都能拷贝
+
+
+//既然是要能拷贝所有类型的内存，就不能像strcpy那样，函数参数是char*,
+//而是用一种通用型的指针来接收，void*
+
+//void *memcpy( void *dest, const void *src, size_t count );
+//count是要从源地址拷贝多少个字节到目的地地址的意思
+
+//#include <stdio.h>
+//
+//struct s
+//{
+//	char name[20];
+//	int age;
+//};
+//
+//int main()
+//{
+//	//拷贝整型数组
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[5] = { 0 };    //目的地地址的数组要确定大小不然会报错
+//
+//	memcpy(arr2, arr1, sizeof(arr1));
+//
+//	int i = 0;
+//
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%d\n", arr2[i]);
+//	}
+//	printf("\n");
+//
+//
+//	//拷贝结构体变量
+//	struct s arr3[] = { {"张三",20},{"李四",30} };
+//	struct s arr4[3] = { 0 };
+//
+//	memcpy(arr4, arr3, sizeof(arr3));
+//
+//	return 0;
+//}
+
+
+
+
+
+//模拟实现memcpy
+
+//#include <stdio.h>
+//#include <assert.h>
+//
+//void* my_memcpy(void* p2, const void* p1, int sz)
+//{
+//	assert(p2 != NULL);
+//	assert(p1 != NULL);
+//
+//	void* ret = p2;
+//
+//	int i = 0;
+//
+//	for (i = 0; i < sz; i++)
+//	{
+//		*(char*)p2 = *(char*)p1;
+//		++(char*)p1;
+//		++(char*)p2;
+//		//优先级：后置++ ＞ 强制类型转换 ＞ 前置++ ，所以用前置++
+//	}
+//
+//	return ret;
+//}
+//
+//struct s
+//{
+//	char name[20];
+//	int age;
+//};
+//
+//int main()
+//{
+//	//打印整型数组
+//	int arr1[] = { 1,2,3,4,5 };
+//
+//	int arr2[10] = { 0 };
+//
+//	int sz = sizeof(arr1);
+//
+//	my_memcpy(arr2, arr1, sz);
+//
+//	int i = 0;
+//
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%d ", arr2[i]);
+//	}
+//	printf("\n\n");
+//
+//
+//	//打印结构体
+//	struct s s1[2] = { {"lisi",30},{"zhangsan",29} };
+//
+//	struct s s2[2] = { 0 };
+//
+//	my_memcpy(s2, s1, sizeof(s1));
+//
+//	printf("%s\n", s2[0].name);
+//	printf("%d\n\n", s2[0].age);
+//	
+//
+//
+//	
+//	//把arr3的12345拷贝到arr3的34567
+//	int arr3[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int* arr4 = arr3 + 2;
+//
+//	my_memcpy(arr4, arr3, 20);
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr3[i]);
+//	}
+//	printf("\n\n");
+//	//这种办法是做不到的，当你要拷贝一个数组到同一个数组时，数组的内容已经改变了，拷贝的结果也不会是预期所想
+//	//有一个专门的函数负责这种内存重叠拷贝的情况，那就是memmove
+//  //当然上面说的做不到只是自己实现的函数做不到,库函数memcpy是可以超额完成任务做到的
+//  //C语言标准:
+//  //memcpy只要处理 不重叠的内存拷贝就可以 - 60 -100
+//  //memmove 处理重叠内存的拷贝 -100
+//
+//	return 0;
+//}
+
+
+
+
+
+
+//memmove 
+//memmove在MSDN的声明:
+//void *memmove( void *dest, const void *src, size_t count );
+//和memcpy一样
+
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//
+//	memmove(arr+2, arr, 20);
+//
+//	int i = 0;
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d\n", arr[i]);
+//	}
+//
+//
+//
+//	return 0;
+//}
+
+
+
+
+//模拟实现memmove
+
+//#include <stdio.h>
+//#include <assert.h>
+//
+//void* my_memmove(void* dest, void* src, size_t count)
+//{
+//	assert(dest != NULL && src != NULL);
+//
+//	void* ret = dest;
+//
+//	
+//	if (src > dest)  //从前往后拷贝
+//	{
+//		while (count--)
+//		{
+//			*(char*)dest = *(char*)src;
+//			++(char*)dest;
+//			++(char*)src;
+//		}
+//	}
+//	else if (src < dest)  //从后向前拷贝
+//	{
+//		while (count--)  //这里count传过来是20，减减进入循环之后就是19，dest和src加上19个字节之后，再解引用，就访问第20个字节
+//		{
+//			*((char*)dest+count) = *((char*)src+count);
+//			//强制转换成char*类型,加上19就是加上19个char,大括号括起来再解引用
+//		}
+//	}
+//
+//	return ret;
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//
+//	my_memmove(arr + 2, arr, 20);
+//
+//	int i = 0;
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//
+//	printf("\n\n");
+//
+//	return 0;
+//}
+
+
+//memcpy和memmove的差别就是memmove函数处理的源内存块和目标内存块是可以重叠的
+//如果源地址和目标地址出现重叠,就得使用memmove函数处理
+
+
+
+
+
+
+
+
+//memcmp
+//memcmp在C语言里的解释:
+//int memcmp( const void *buf1, const void *buf2, size_t count );
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5,6 };
+//	int arr2[] = { 1,2,3,5,5,6 };
+//
+//	int ret = memcmp(arr1, arr2,13);  //13是要比较13个字节的意思
+//	//第一个大于第二个返回1
+//	//第一个小于第二个返回-1
+//	//第一个等于第二个返回0
+//
+//	printf("%d\n", ret);
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+//memset - 内存设置
+//void *memset( void *dest, int c, size_t count );
+
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	char arr[10] = "0";
+//
+//	memset(arr, '#', 10);
+//
+//	printf("%s\n", arr);
+//
+//	int arr1[10] = { 0 };
+//
+//	memset(arr1, '1', 10);
+//	//这里的10把前10个字节变成1，而不是把10个元素都变成1
+//	//如果要把每个元素都初始化为1，那就要设定成40
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+//C语言进阶第四章：自定义类型
+
+//C语言内置类型：char short int long float double
+//C语言自定义类型：结构体，枚举，联合
+
+
+//结构体：
+//结构体类型的声明
+//结构体的自引用
+//结构体变量的定义和初始化
+//结构体内存对齐
+//结构体传参
+//结构体实现位段(位段的填充&可移植性)
+
+
+
+//结构体的声明:
+//结构是一些值的集合，这些值称为成员变量。结构的每个成员可以是不同类型的变量
+//(联想):数组是一组相同元素类型的元素的集合
+
+//声明一个学生类型，想通过学生类型来创建学生变量(对象)
+
+//#include <stdio.h>
+//
+//struct t
+//{
+//	int abc;
+//	char c;
+//};
+//
+//struct stu  //struct-结构体关键字  stu-结构体标签  struct stu-结构体类型
+//{
+//	struct t T1;      //结构体创建变量有四种方法:s1,s2,s3,s4和T1
+//	char name[20];
+//	char tele[12];
+//	char sex[10];
+//	int age;
+//	//以上是结构体成员变量
+//}s4;
+//
+//struct stu s3;
+//
+//int main()
+//{
+//
+//	//通过结构体类型创建结构体变量,由于变量放在main内部，所以是局部变量
+//	//如果想创建结构体全局变量，那就放在main外面,也就是结构体类型的最后面，分号前
+//	//也可以直接在main函数外面声明，不在struct类型上声明
+//	//s1,s2,s3,s4都是结构体变量，s1和s2是结构体局部变量,s3和s4是结构体全局变量
+//
+//	struct stu s1;
+//	struct stu s2;
+//
+//	return 0;
+//}
+
+
+
+
+
+//匿名结构体类型
+
+//#include <stdio.h>
+//
+//struct     //-当结构体没有标签的时候，这个结构体类型就是匿名结构体类型
+//	       //匿名结构体变量创建时可以没有标签，但是在后面必须跟一个名字来创建匿名结构体变量
+//{
+//	int a;
+//	char c;
+//}sa;
+//
+//struct
+//{
+//	int a;
+//    char c;
+//}*psa;     //在匿名结构体名前面加*，就是匿名结构体指针类型
+//
+////上面的两个结构在声明的时候省略掉了结构体标签(tag)
+//
+//int main()
+//{
+//	*psa = &sa;
+//	//这种写法会报错
+//	//sa和psa是两个成员完全相同的匿名结构体,但是psa匿名结构体指针放不了sa的地址
+//	//编译器会认为这是两个完全不同的变量
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+//结构体的自引用
+//这里要搞懂，数据结构里面有
+
+//在结构体中包含一个类型为该结构本身的成员可以吗？
+//答案是不可以,这样大小是不能确定的
+//正确自引用方式是结构体要能够找到同类型的其他的变量，用指针把它串联起来
+//指针指向的内容也许很大，但指针永远只有4/8个字节，所以用指针来串
+
+
+//#include <stdio.h>
+//
+//struct Node
+//{
+//	int data;
+//	struct Node n;
+//};
+//数据结构：数据在内存中存储的结构
+//链表:创建了一个int类型的数组:12345,这12345是通过链条连接起来的，1的结点包含了1以及下一个元素(也就是2)的地址
+//5的节点包含了5和\0
+//如果要创建这样一个链表节点的结构体变量，那一个节点除了包含自身之外，还要包含下一个节点的地址
+
+//struct Node
+//{
+//	//一个节点存放两个部分，一个部分是数据，一个部分是下一个节点的地址
+//	//存放数据的叫做数据域，存放地址的叫做指针域
+//	int date;
+//	struct Node* next;
+// //结构体要找到一个同类型的变量的话，用指针去找
+//};
+//
+//int main()
+//{
+//	return 0;
+//}
+
+
+
+
+
+//结构体自引用的时候不能省略类型的创建，即使是typedef
+//结构体改名 typedef
+
+//#include <stdio.h>
+//
+//typedef struct Node   
+//{
+//	int data;
+//	struct Node* next;
+//
+//}Node;  //-用typedef把结构体类型struct Node改名为Node
+//
+//
+////typedef struct
+////{
+////	int data;
+////	Node* next;
+////}Node;
+////这个匿名结构体代码会报错,代码从上到下运行,Node还没有重命名，结构体就有一个Node了,会报错
+////所以重命名的时候结构体标签不能省略掉
+//
+//int main()
+//{
+//	struct Node s1;  //-可以正常创建变量
+//	Node s2;         //-也可以用typedef的Node来创建变量
+//	return 0;
+//}
+
+
+
+
+
+//结构体变量的定义和初始化
+
+//#include <stdio.h>
+//
+//struct t
+//{
+//	int abc;
+//    char c;
+//};
+//
+//struct s
+//{
+//	char c;
+//	struct t T1;
+//	int a;
+//	double d;
+//	char arr[20];
+//};
+//
+//int main()
+//{
+//	struct s s1 = { 'c',{10,'b'},200,3.14,"lily"};
+//	//给结构体每个成员一个值就是结构体初始化
+//
+//	printf("%c\n%d\n%lf\n%s\n\n", s1.c, s1.a, s1.d, s1.arr);
+//	//要把结构体每个成员拿出来就涉及到结构体成员的访问
+//
+//	//打印结构体里面的结构体
+//	printf("%d\n%c\n\n", s1.T1.abc, s1.T1.c);
+//
+//	//通过访问指针打印结构体
+//	struct s* s2 = &s1;
+//	printf("%c\n", s2->c);
+//	printf("%c\n\n", (*s2).c);
+//
+//	//通过指针打印结构体里的结构体
+//	printf("%d\n%c\n", s2->T1.abc, s2->T1.c);
+//	//s2->T1是通过结构体指针访问这个成员，这个成员又是结构体，就用.来访问
+//	//结构体指针访问用->		结构体访问用.
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+//结构体内存对齐
+
+
+//以下代码输出是多少
+
+//#include <stdio.h>
+//
+//struct s1
+//{
+//	char c1;
+//	int a;
+//	char c2;
+//};
+//
+//struct s2
+//{
+//	char c1;
+//	char c2;
+//	int a;
+//};
+//
+//int main()
+//{
+//	struct s1 S1 = { 0 };
+//	struct s2 S2 = { 0 };
+//
+//	printf("%d\n", sizeof(S1));
+//	printf("%d\n", sizeof(S2));
+//
+//	return 0;
+//}
+
+//如何计算结构体大小？
+// 1.第一个成员在与结构体变量偏移量为0的地址处
+// 2.其他成员变量要对齐到某个数字(对齐数)的整数倍的地址处
+//对齐数 = 编译器默认的一个对齐数  与 该成员大小的较小值
+//VS中默认的值为8		在gcc编译器里成员的大小就是对齐数
+//
+// 3.结构体总大小为最大对齐数(每个成员变量都有一个对齐数)的整数倍
+// 4.如果嵌套了结构体的情况，嵌套的结构体对齐到自己的最大对齐数的整数倍处,
+// 结构体的整体大小就是所有最大对齐数(含嵌套结构体的对齐数)的整数倍
+
+// 结构体内部内存，按照最大的类型进行运算
+// 结算的时候必须是最大类型的整数倍，不足整数倍的则补足至整数倍。
+
+
+
+
+//作业：下面代码输出多少？
+
+//#include <stdio.h>
+//
+//struct s3
+//{
+//	double b;
+//	char c;
+//	int a;
+//};
+//
+//struct s4
+//{
+//	char c1;
+//	struct s3 S33;
+//	double d;
+//};
+//
+//int main()
+//{
+//	struct s3 S3 = { 3.14,'c',10 };
+//	printf("%d\n", sizeof(S3));
+//
+//
+//	struct s4 S4 = { 'c',{3.14,'c',10},9.6 };
+//	printf("%d\n", sizeof(S4));
+//	// 4.如果嵌套了结构体的情况，嵌套的结构体对齐到自己的最大对齐数的整数倍处,
+//	// 结构体的整体大小就是所有最大对齐数(含嵌套结构体的对齐数)的整数倍
+//
+//	return 0;
+//}
+
+
+
+//为什么会存在内存对齐
+//1.平台原因(移植原因):不是所有的硬件平台都能访问任意地址上的任意数据的；
+// 某些硬件平台只能在某些地址处取某些特定类型的数据，否则抛出硬件异常
+//2.性能原因：数据结构(尤其是栈)应该尽可能地在自然边界上对齐。
+// 原因在于，为了访问未对齐的内存，处理器需要作两次内存访问；而对齐的内存访问仅需要一次访问
+
+//总的来说：结构体的内存对齐是拿 空间 来换 时间 的做法
+
+
+
+
+
+//那在设计结构体的时候，我们既要满足对齐，又要节省空间，如何做到：
+//办法就是：让占用空间小的成员尽量集中在一起
+
+
+//例如：
+//struct s1
+//{
+//	char c1;
+//	int i;
+//	char c2;
+//};
+//
+//struct s2
+//{
+//	char c1;
+//	char c2;
+//	int i;
+//};
+
+//s1和s2类型成员一模一样，但是s1和s2所占空间的大小却不一样,s1的空间要更小
+
+
+
+
+
+
+
+
+//修改默认对齐数
+//#pragma这个预处理指令，可以改变我们的默认对齐数
+
+//#include <stdio.h>
+//
+//#pragma pack(4)  //设置默认对齐数为4
+//
+//struct s
+//{
+//	char c1;
+//	double b;
+//};
+//
+////#pragma pack()  //取消设置的默认对齐数
+//
+//int main()
+//{
+//	struct s s1;
+//
+//	printf("%d\n", sizeof(s1));
+//	//如果默认对齐数是8，那么结果就是16
+//	//默认对齐数是1，结果就是9
+//
+//    
+//
+//	printf("%d\n", sizeof(s1));
+//
+//	return 0;
+//}
+
+
+//结构体在对齐方式不合适的时候，可以自己更改默认对齐数
+
+
+
+
+
+
+
+//offsetof - 作用是 计算结构体成员相对于起始位置的偏移量
+//offsetof是一个宏，不是函数
+
+//在MSDN的定义：size_t offsetof( structName, memberName );
+//第一个参数是结构体类型名(不是变量名)，第二个参数是成员名,返回的是偏移量
+
+//#include <stdio.h>
+//#include <stddef.h>
+//
+//struct s
+//{
+//	char c;
+//	int i;
+//	double d;
+//};
+//
+//int main()
+//{
+//	printf("%d\n", offsetof(struct s, c));
+//	printf("%d\n", offsetof(struct s, i));
+//	printf("%d\n", offsetof(struct s, d));
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+//结构体传参
+
+//#include <stdio.h>
+//
+//struct S
+//{
+//	int a;
+//	char c;
+//	double d;
+//};
+//
+//void Init(struct S* s)
+//{
+//	s->a = 10;
+//	s->c = 'b';
+//	s->d = 3.14;
+//}
+//
+//void print1(struct S* s)
+//{
+//	printf("%d\n", s->a);
+//	printf("%c\n", s->c);
+//	printf("%lf\n", s->d);
+//}
+//
+//void print2(struct S tmp)
+//{
+//	printf("%d\n", tmp.a);
+//	printf("%c\n", tmp.c);
+//	printf("%lf\n", tmp.d);
+//}
+//
+//int main()
+//{
+//	struct S s;
+//	//赋值
+//	Init(&s);
+//
+//	//结构体传址 打印
+//	print1(&s);
+//
+//	//结构体传值 打印
+//	print2(s);
+//
+//	return 0;
+//}
+
+//结构体传参，要穿结构体的地址
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//枚举：
+//枚举类型的定义
+//枚举的优点
+//枚举的使用
+
+
+//联合
+//联合类型的定义
+//联合的特点
+//联合大小的计算
